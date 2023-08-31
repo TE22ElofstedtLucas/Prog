a73bc76016f6c46c5301d3a1ee937b3343328d91
Lucas Löfstedt <Lucas.bronegardlofstedt@elev.ga.ntig.se>
2023-08-31 10:32:01 +0200

    hej

---
+++ Lucktext/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+string name = "";
+string favoritmat = "";
+string längd = "";
+
+Console.WriteLine("Skriv in ditt namn:");
+name = Console.ReadLine();
+
+Console.WriteLine("Skriv in din favoritmat:");
+favoritmat = Console.ReadLine();
+
+Console.WriteLine("Skriv in din längd:");
+längd    = Console.ReadLine();
+
+
